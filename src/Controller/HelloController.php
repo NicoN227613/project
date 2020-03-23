79470b89b92881d0b0dbf58a84aaeba05d7f7abc
NicoN227613 <nicolasn227613@gmail.com>
2020-03-23 15:36:48 +0100

    Premier contrôleur

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class HelloController
+{
+    public function hello()
+    {
+        $response = new Response();
+        $response->setContent('Bonjour tout le monde!'."\n");
+        $response->headers->set('Content-Type', 'text/plain');
+        $response->setStatusCode(Response::HTTP_PARTIAL_CONTENT);
+
+        return $response;
+    }
+}
