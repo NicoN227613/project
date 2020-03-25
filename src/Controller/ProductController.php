1559d2e6ac52fd4c2aa3001e401142f8c2b5cf69
NicoN227613 <nicolasn227613@gmail.com>
2020-03-25 10:18:56 +0100

    Formulaire produit

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Product;
 use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -36,31 +38,56 @@ class ProductController extends AbstractController
     }
 
     /**
-     * @Route("/new")
+     * @Route("/new", methods={"GET", "POST"})
      */
-    public function new()
+    public function new(Request $request, EntityManagerInterface $manager)
     {
         $product = new Product();
 
         $form = $this->createFormBuilder($product)
-            ->add('name')
-            ->add('category')
-            ->add('emplacement')
-            ->add('quantity')
-            ->add('unity')
+            ->add('name', null, [
+                'label' => 'Nom'
+            ])
+            ->add('category', null, [
+                'label' => 'Catégorie'
+            ])
+            ->add('emplacement', null, [
+                'label' => 'Emplacement'
+            ])
+            ->add('quantity', null, [
+                'label' => 'Quantité'
+            ])
+            ->add('unity', null, [
+                'label' => 'Unité'
+            ])
             ->add('purchaseDate', DateType::class, [
                 'html5' => true,
                 'widget' => 'single_text',
+                'label' => 'Acheté le'
             ])
             ->add('expirationDate', DateType::class, [
                 'html5' => true,
                 'widget' => 'single_text',
+                'label' => 'A consommer avant le'
             ])
             ->add('bestBeforeDate', DateType::class, [
                 'html5' => true,
                 'widget' => 'single_text',
+                'label' => 'A consommer de préférence avant le'
             ])
             ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($product);
+            $manager->flush();
+
+            $this->addFlash('success', 'Nouveau produit créé');
+            return $this->redirectToRoute('app_product_show', [
+                'id' => $product->getId(),
+            ]);
+        }
 
         return $this->render('product/new.html.twig', [
             'new_form' => $form->createView(),
