bcde076b1fa50586c9bcce89d895295cf512531d
NicoN227613 <nicolasn227613@gmail.com>
2020-04-09 22:21:36 +0200

    new, edit, delete product côte user

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,22 +3,31 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Form\ProductType;
 use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
 * @Route("/product")
 */
 class ProductController extends AbstractController
 {
+    public function __construct(ProductRepository $repository, EntityManagerInterface $manager)
+    {
+        $this->repository = $repository;
+        $this->manager = $manager;
+    }
+
     /**
     * @Route("", methods="GET")
     */
-    public function index(ProductRepository $repository)
+    public function index()
     {
-        $products = $repository->findWithCategories();
+        $products = $this->repository->findWithCategories();
 
         return $this->render('product/index.html.twig', [
             'products' => $products,
@@ -35,5 +44,64 @@ class ProductController extends AbstractController
         return $this->render('product/show.html.twig', [
             'product' => $product,
         ]);
+    }
+    
+    /**
+     *@Route("/new")
+     */
+    public function new(Request $request)
+    {
+        $product = new Product();
+        $product->setAuthor($this->getUser());
+        $form = $this->createForm(ProductType::class, $product);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $this->manager->persist($product);
+            $this->manager->flush();
+            $this->addFlash('success', 'Votre produit ' . $product->getName() . ' a bien était crée !');
+            return $this->redirectToRoute('app_product_index');
+        }
+
+        return $this->render('product/new.html.twig', [
+            'product' => $product,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     *@Route("/{id}/edit", requirements={"id": "\d+"}, methods={"GET", "PUT"})
+     */
+    public function edit(Product $product, Request $request)
+    {
+        $form = $this->createForm(ProductType::class, $product, [
+            'method' => 'PUT',
+        ]);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $this->manager->flush();
+            $this->addFlash('success', 'Votre produit ' . $product->getName() . ' a bien était modifié !');
+            return $this->redirectToRoute('app_product_index');
+        }
+
+        return $this->render('product/edit.html.twig', [
+            'product' => $product,
+            'form' => $form->createView()
+        ]);
+    }
+    /**
+     *@Route("/product/{id}", requirements={"id": "\d+"}, methods="DELETE")
+     */
+    public function delete(Product $product, Request $request)
+    {
+        if($this->isCsrfTokenValid('delete' . $product->getId(), $request->get('_token'))){
+            $this->manager->remove($product);
+            $this->manager->flush();
+            $this->addFlash('success', 'Votre produit ' . $product->getName() . ' a bien était supprimé!');
+        }
+        return $this->redirectToRoute('app_product_index');
     }
 }
