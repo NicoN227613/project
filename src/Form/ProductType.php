e8df67918b7a887e1ef5402d85b89367989acd61
NicoN227613 <nicolasn227613@gmail.com>
2020-04-28 16:51:07 +0200

    Mise en place nouveau type de champs permettant d'associer un utilisateur à un produit avec un système de recherche en ajax + css

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -8,6 +8,7 @@ use App\Entity\Product;
 use App\Entity\Category;
 use App\Entity\Emplacement;
 use App\Form\Type\DateType;
+use App\Form\Type\ChoicesType;
 use App\Form\Type\UserChoiceType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
